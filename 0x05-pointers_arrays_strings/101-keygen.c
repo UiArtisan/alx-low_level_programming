bf8bb85e4189b06146ac5422072f161b07360bb7
Zakaria Oubella <153244002+UiArtisan@users.noreply.github.com>
2024-07-01 21:54:50 +0100

    feat: Add random password generator for 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,15 +9,15 @@
 
 int main(void)
 {
-	int sum = 0;
-	char ch;
+    int sum = 0;
+    char ch;
 
-	srand(time(NULL));
-	do {
-		ch = rand() % 128;
-		sum += ch;
-		putchar(ch);
-	} while (sum <= 2645);
-	putchar(2772 - sum);
-	return (0);
+    srand(time(NULL));
+    while (sum <= 2645 - 127) {  // Ensure we don't exceed the required sum
+        ch = rand() % 94 + 33;  // Generate a printable ASCII character (from '!' to '~')
+        sum += ch;
+        putchar(ch);
+    }
+    putchar(2772 - sum);  // Output the last character to complete the required sum
+    return (0);
 }
