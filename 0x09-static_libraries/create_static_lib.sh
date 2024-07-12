3a384488eb56f9851c9392ada0d1ba2eafc1fb85
Zakaria Oubella <153244002+UiArtisan@users.noreply.github.com>
2024-07-13 00:42:11 +0100

    Add create_static_lib.sh script to create a static library from .c files

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-gcc -c *.c
+
+# Compile all .c files into object files
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
+
+# Create the static library from the object files and index it
 ar rcs liball.a *.o
-rm *.o
