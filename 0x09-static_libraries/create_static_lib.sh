726d8ae58ea9536163023b34050108d0906c1697
Zakaria Oubella <153244002+UiArtisan@users.noreply.github.com>
2024-07-13 00:36:20 +0100

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Compile all .c files into object files
+# Create object files for each .c file in the current directory
 gcc -c *.c
 
-# Create the static library from the object files and index it
+# Create the static library from the object files
 ar rcs liball.a *.o
+
+# Clean up by removing the object files
+rm *.o
